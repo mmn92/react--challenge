59dbfc08e145b6c430a3b9503a2a70c768ab3c25
ITERIS\matheus.nogueira <matheus.nogueira@iteris.local>
2020-05-08 12:03:52 -0300

    update project to use new hooks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,88 @@
-import React, { useState, useEffect } from "react";
-import api from './services/api';
+import React, { useReducer, useEffect } from "react";
+import { usePaginatedList } from './hooks/useList';
 
 import "./styles.css";
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'SET_COUNT':
+      return {...state, count: action.payload}
+    case 'SET_PAGE':
+      return {...state, page: action.payload}
+    case 'SET_EXPANDED':
+        return {...state, expanded: action.payload}  
+    default:
+      return {...state}
+  }
+}
+
 function App() {
-  const [repos, updateRepos] = useState([])
+  const [state, dispatch] = useReducer(reducer, {page: 1, count: 0, expanded: 0})
+  const paginatedList = usePaginatedList(state.page)
 
   useEffect(() => {
-    const fetchData = async () => {
-      const result = await api.get('/repositories');
- 
-      updateRepos(result.data);
-    };
- 
-    fetchData();
-  }, []);
+    dispatch({type: 'SET_COUNT', payload: state.page})
+  }, [state.page])
 
-  async function handleAddRepository() {
-    const newRepo = {
-      title: "title",
-      url: "https://url.com",
-      techs: []
-    }
-    
-    const response = await api.post("/repositories", newRepo)
-    
-    updateRepos([...repos, response.data])
-  }
+  useEffect(() => {
+    dispatch({type: 'SET_EXPANDED', payload: 0})
+  }, [paginatedList.resolvedData])
 
-  async function handleRemoveRepository(id) {
-    api.delete(`/repositories/${id}`);
-    
-    updateRepos(repos.filter(repo => repo.id !== id))
+  if(paginatedList.status === 'error') {
+    return <div>Error: {paginatedList.error}</div>
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        {repos.map((repo, index) => (
-          <li key={index}>
-            {repo.title}
-            <button onClick={() => handleRemoveRepository(repo.id)}>
-              Remover
-            </button>
-          </li>
-        ))}
-      </ul>
+      {paginatedList.status === 'success' && <>
+        <span>Page {state.count}</span>
+        <div>
+          <ul data-testid="repository-list">
+            {paginatedList.resolvedData.data.results.map((repo, index) => (
+              <li key={index}>
+                {repo.name}
+                <button onClick={() => dispatch({type: 'SET_EXPANDED', payload: index})}>
+                  More
+                </button>
+              </li>
+            ))}
+          </ul>
+          <div style={{
+            display: 'flex', 
+            flexDirection: 'column', 
+            justifyContent: 'center', 
+            alignItems: 'center',
+            width: '250px', 
+            padding: '15px', 
+            border: '3px solid purple',
+            borderRadius: '8px',
+            background: '#7159c1',
+            color: '#fff'
+            }}>
+            <span>{paginatedList.resolvedData.data.results[state.expanded].name}</span>
+            <span>{paginatedList.resolvedData.data.results[state.expanded].gender}</span>
+            <span>{paginatedList.resolvedData.data.results[state.expanded].height}</span>
+          </div>
+        </div>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+        <div>
+          <button 
+            disabled={paginatedList.isFetching || state.page === 1}
+            onClick={() => {
+              dispatch({type: 'SET_PAGE', payload: state.page - 1})
+            }}>Prev</button>{' '}
+          <button 
+            disabled={paginatedList.isFetching}
+            onClick={() => {
+              dispatch({type: 'SET_PAGE', payload: state.page + 1})
+            }}>Next</button>
+        </div>
+        {paginatedList.isFetching && <span>Loading more...</span>}
+        </>}
     </div>
   );
+
+
 }
 
 export default App;
